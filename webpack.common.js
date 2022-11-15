d40f3d4b81ee274097a78633474b5ad63a794a4d
Salvation_sub <kyman19940214@gmail.com>
2022-11-15 16:23:59 +0900

    feat: Typescript

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,21 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        app: './src/index.js',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Production',
-        }),
-    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
     output: {
-        filename: '[name].bundle.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true,
     },
 };
