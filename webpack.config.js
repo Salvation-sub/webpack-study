f5a3a16485676a8398f506b649290c88e5e65a66
Salvation_sub <kyman19940214@gmail.com>
2022-11-15 14:42:32 +0900

    feat: Hot Module Replacement(HMR)

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        // hot module replacement를 위한 런타임 코드
+        hot: 'webpack/hot/dev-server.js',
+        // 웹 소켓 전송, hot 및 live 리로드 로직을 위한 개발 서버 클라이언트
+        client: 'webpack-dev-server/client/index.js?hot=true&live-reload=true',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        // 웹 소켓 전송, hot 및 live 리로드 로직을 위한 개발 서버 클라이언트
+        hot: false,
+        client: false,
+    },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Caching',
+            title: 'Hot Module Replacement',
         }),
+        // hot module replacement를 위한 플러그인
+        new webpack.HotModuleReplacementPlugin(),
     ],
     output: {
-        filename: '[name].[contenthash].js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-    },
-    optimization: {
-        runtimeChunk: 'single',
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                },
-            },
-        },
     },
 };
